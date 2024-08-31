e7f8782b18aff7a774c248415e6cb64ed700f7bd
Joseph Liyon PT <120583161+jliyon23@users.noreply.github.com>
2024-08-31 18:59:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/bypass-recaptcha', methods=['GET'])
 def bypass_recaptcha():
-    anchorr = "https://www.google.com/recaptcha/api2/anchor?ar=1&k=6Ldb0ioqAAAAAJMH5vs0_SAPK72nf7hEE5R9wpmf&co=aHR0cHM6Ly9rdHUuZWR1LmluOjQ0Mw..&hl=en-GB&v=i7X0JrnYWy9Y_5EYdoFM79kV&size=invisible&cb=aa13domwesgy".strip()
+    anchorr = "https://www.google.com/recaptcha/api2/anchor?ar=1&k=6LehQTMqAAAAAKcRUGEQAcNFU8gsYSkiadA8CNkV&co=aHR0cHM6Ly9rdHUuZWR1LmluOjQ0Mw..&hl=en-GB&v=WV-mUKO4xoWKy9M4ZzRyNrP_&size=invisible&cb=jbrmjrpxpyo5".strip()
 
     if not anchorr:
         return jsonify({'error': 'Anchor URL is required'}), 400
